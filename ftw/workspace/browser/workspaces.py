3ed56b8d68b83913ea6ea6d3ee4c8d66ece0c64f
Victor Baumann <v.baumann@4teamwork.ch>
2010-08-09 09:02:13 +0200

    calendar tab and workspaces view

---
+++ ftw/workspace/browser/workspaces.py
@@ -0,0 +1,27 @@
+from ftw.tabbedview.browser.tabbed import TabbedView
+from ftw.table import helper
+from ftw.workspace.browser import helper as workspace_helper
+from tab import Tab
+
+
+
+class WorkspacesView(TabbedView):
+    """Tabbed workspaces overview"""
+
+    def get_tabs(self):
+        return [{'id':'workspaces', 'class':''},
+                {'id':'documents','class':''},
+                {'id':'events','class':''},
+               ]
+
+
+class WorkspacesTab(Tab):
+
+   types = 'Workspace'
+
+   sort_on = 'sortable_title'
+
+   columns = (('', helper.path_checkbox),
+              ('Title', 'sortable_title', helper.linked),
+              ('modified', helper.readable_date),
+              ('Creator', 'sortable_creator', helper.readable_author), )
