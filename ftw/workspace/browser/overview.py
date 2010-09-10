da41bc9c6102fac1e29c2770d51f8b2c13169309
Jonas Baumann <jone@jone.ch>
2010-09-10 08:59:14 +0200

    cleanup

--- ftw/workspace/browser/overview.py
+++ ftw/workspace/browser/overview.py
@@ -3,27 +3,18 @@ from Products.Five.browser import BrowserView
 
 class OverviewTab(BrowserView):
 
-    def catalog(self, types, depth=2, sort_on = 'modified'):
+    def catalog(self, types, depth=2, sort_on='modified'):
         return self.context.portal_catalog(
             portal_type=types,
-            path=dict(
-                depth=depth,
-                query='/'.join(self.context.getPhysicalPath())),
-                sort_on=sort_on,
-                sort_order='reverse')
-
-    # def boxes(self):
-    #     items = [[dict(id = 'folders', content=self.folders(), type_='list'),
-    #               dict(id='description', content=self.description()), ],
-    #               [dict(id ='documents', content=self.documents()),
-    #               dict(id='recently_modified', content=self.recently_modified()),
-    #             ]]
-    #     return items
+            path=dict(depth=depth,
+                      query='/'.join(self.context.getPhysicalPath())),
+            sort_on=sort_on,
+            sort_order='reverse')
 
     def folders(self):
         return self.catalog(
-            ['Folder', 'Workspace', 'TabbedViewFolder' ],
-            sort_on = 'created')[:-1]
+            ['Folder', 'Workspace', 'TabbedViewFolder'],
+            sort_on='created')[:-1]
 
     def blogs(self):
         return self.catalog(['Blog', ])
@@ -32,9 +23,9 @@ class OverviewTab(BrowserView):
         return self.context.Description()
 
     def documents(self):
-        return self.catalog(['File', ], sort_on='created', )[:5]
+        return self.catalog(['File', ], sort_on='created')[:5]
 
     def recently_modified(self):
         return self.catalog(['File', 'Folder',
-                            'Workspace', 'Event', ],
-                            sort_on='created', )[:5]
+                             'Workspace', 'Event', ],
+                            sort_on='created')[:5]
