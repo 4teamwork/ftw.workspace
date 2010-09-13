5af0e251bef74c2c3ba98b8c1144ec4f4c576c93
Jonas Baumann <jone@jone.ch>
2010-09-13 09:13:34 +0200

    Issue #329 Termine am 1-9. Tag vom Monat werden nicht korrekt angezeigt   https://extranet.4teamwork.ch/intranet/10-interne-projekte/4teamwork-egov/tracker-4teamwork-egov/329

--- ftw/workspace/browser/resources/event-calendar.js
+++ ftw/workspace/browser/resources/event-calendar.js
@@ -16,6 +16,9 @@ jq(function() {
      jq('td.event a,  td.todayevent a').live(
        'click',
        function(e, o) {
+         // tell the calendar tooltip to hide, because we will hide
+         // the link and switch view...
+         jq(this).trigger('mouseleave');
          tabbedview.searchbox.val(this.id);
          tabbedview.prop('searchable_text', this.id);
          tabbedview.param('view_name', 'events');
