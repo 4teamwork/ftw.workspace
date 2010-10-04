5168f17d8a1b55f05fb5a53053b56a13433de8c3
Timon Tschanz <timon.tschanz@4teamwork.ch>
2010-10-04 13:54:03 +0200

    fixed bug in Overview

--- ftw/workspace/browser/overview.py
+++ ftw/workspace/browser/overview.py
@@ -3,25 +3,23 @@ from ftw.workspace.browser import helper as workspace_helper
 
 class OverviewTab(BrowserView):
 
-    def catalog(self, types, depth=2, sort_on='modified'):
+    def catalog(self, types, depth=2, sort_on='modified', sort_order='reverse'):
         return self.context.portal_catalog(
             portal_type=types,
             path=dict(depth=depth,
                       query='/'.join(self.context.getPhysicalPath())),
             sort_on=sort_on,
-            sort_order='reverse')
+            sort_order=sort_order)
 
     def folders(self):
         # import pdb; pdb.set_trace( )
         subfolders = []
+        folderobjects = []
         all_folders = self.catalog(
-            ['Folder', 'Workspace', 'TabbedViewFolder'],
-            sort_on='created')[:-1]
-        for item in all_folders:
-            folderObject = item.getObject()
-            if folderObject.getParentNode() == self.context:
-                subfolders.append(item)
-        return subfolders
+            ['Folder', 'Workspace', 'TabbedViewFolder'], depth=1,
+            sort_on = 'sortable_title', sort_order = '')
+        return all_folders
+
     def blogs(self):
         return self.catalog(['Blog', ])
 
