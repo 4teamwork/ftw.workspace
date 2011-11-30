c8e7694b7812a8801fc68b24759c5f614df8f19f
Jonas Baumann <jone@jone.ch>
2011-11-30 15:35:58 +0100

    Move version from version.txt to setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/workspace/version.txt').read().strip()
+version = '1.3.4dev'
 
 tests_require = [
     'plone.app.testing',
