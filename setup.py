3ae13327d1f8ce3c4f8fc819e686e1f4b3354aa6
Jonas Baumann <jone@jone.ch>
2011-11-30 15:42:16 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -5,39 +5,50 @@ version = '1.3.4dev'
 
 tests_require = [
     'plone.app.testing',
-]
+    ]
 
 
 setup(name='ftw.workspace',
       version=version,
-      description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      description='A project folder for plone.',
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
         ],
-      keywords='',
-      author='Victor Baumann',
-      author_email='v.baumann4teamwork.ch',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
+
+      keywords='ftw workspace project folder plone',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.workspace/',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'ftw.tabbedview',
-          'plone.principalsource',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'ftw.tabbedview',
+        'plone.principalsource',
+        # -*- Extra requirements: -*-
+        ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       test_suite='ftw.workspace.tests.test_docs.test_suite',
-      entry_points="""
+      entry_points='''
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
+      ''',
       )
