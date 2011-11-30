27169781f4c823f3c0f62803b9b8f06e1a06ad32
Jonas Baumann <jone@jone.ch>
2011-11-30 15:36:50 +0100

    Rename readme to .rst

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 setup(name='ftw.workspace',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
