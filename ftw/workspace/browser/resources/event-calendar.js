f9bc9764d0be59542ac92dc657cefbf7c70d32fc
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-02-04 09:52:36 +0100

    Replace jq by $

--- ftw/workspace/browser/resources/event-calendar.js
+++ ftw/workspace/browser/resources/event-calendar.js
@@ -1,11 +1,11 @@
-jq(function() {
+$(function() {
 
      /*calander navigation*/
-     jq('a.#calendar-previous, a.#calendar-next').live(
+     $('a.#calendar-previous, a.#calendar-next').live(
        'click',
        function(e, o) {
-         var month = jq.find_param(this.href)['amp;month:int'];
-         var year = jq.find_param(this.href)['year:int'];
+         var month = $.find_param(this.href)['amp;month:int'];
+         var year = $.find_param(this.href)['year:int'];
          tabbedview.param('month:int', month);
          tabbedview.param('year:int', year);
          tabbedview.reload_view();
@@ -13,12 +13,12 @@ jq(function() {
          e.stopPropagation();
        });
 
-     jq('td.event a,  td.todayevent a').live(
+     $('td.event a,  td.todayevent a').live(
        'click',
        function(e, o) {
          // tell the calendar tooltip to hide, because we will hide
          // the link and switch view...
-         jq(this).trigger('mouseleave');
+         $(this).trigger('mouseleave');
          tabbedview.searchbox.val(this.id);
          tabbedview.prop('searchable_text', this.id);
          tabbedview.param('view_name', 'events');
@@ -27,12 +27,12 @@ jq(function() {
          e.stopPropagation();
        });
 
-     jq('.tabbedview_view').bind(
+     $('.tabbedview_view').bind(
        'reload',
        function(e, o) {
 
       /*calendar tooltip*/
-             jq('td.event a, td.todayevent a').tooltip(
+             $('td.event a, td.todayevent a').tooltip(
                  {showURL: false,
                   track: true,
                   fade: 250,
