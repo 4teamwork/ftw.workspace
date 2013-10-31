4e20a4a3eedf9f147cdb058a1dd329387d495fe2
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-10-31 17:06:53 +0100

    Use portal_url in default adapter (browser caching)

--- ftw/workspace/browser/previews/default.py
+++ ftw/workspace/browser/previews/default.py
@@ -1,6 +1,7 @@
 from ftw.workspace import _
 from ftw.workspace.interfaces import IWorkspacePreview
 from plone.namedfile.interfaces import IAvailableSizes
+from Products.CMFCore.utils import getToolByName
 from zope.component import adapts
 from zope.component import getUtility
 from zope.interface import implements
@@ -18,14 +19,15 @@ class DefaultPreview(object):
         self.request = request
 
     def preview(self):
-        return '<img height="200px" src="{0}" alt={1} />'.format(
+        return '<img height="200px" src="{0}" alt={1} title={1}/>'.format(
             self.full_url(),
             _(u'text_no_preview', default=u'No Preview')
             )
 
     def full_url(self):
+        portal_url = getToolByName(self.context, 'portal_url')
         return '{0}/++resource++ftw.workspace-resources/default.jpeg'.format(
-            self.context.absolute_url())
+            portal_url())
 
     def get_scale_properties(self):
         sizes = getUtility(IAvailableSizes)()
