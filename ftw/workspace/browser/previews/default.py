951abe41847854355595c06ecc981f4e6e9ed25b
Lukas Knoepfel <shylux@gmail.com>
2014-05-01 16:54:30 +0200

    Added detail url in preview.

--- ftw/workspace/browser/previews/default.py
+++ ftw/workspace/browser/previews/default.py
@@ -19,9 +19,10 @@ class DefaultPreview(object):
         self.request = request
 
     def preview(self):
-        return '<img height="200px" src="{0}" alt="{1}" title="{1}" download_url="{2}" />'.format(
+        return '<img height="200px" src="{0}" alt="{1}" title="{1}" detail_url="{2}" download_url="{3}" />'.format(
             self.full_url(),
             _(u'text_no_preview', default=u'No Preview'),
+            self.detail_url(),
             self.download_url()
             )
 
@@ -33,6 +34,16 @@ class DefaultPreview(object):
     def get_scale_properties(self):
         sizes = getUtility(IAvailableSizes)()
         return sizes.get('workspace_preview', (200, 200))
+
+    def detail_url(self):
+        properties = getToolByName(self.context, 'portal_properties')
+        view_action_types = properties.site_properties.getProperty(
+            'typesUseViewActionInListings', ())
+        url = self.context.absolute_url()
+        if self.context.portal_type in view_action_types:
+            url = '%s/view' % url
+
+        return url
 
     def download_url(self):
         return "%s/download" % self.context.absolute_url()
