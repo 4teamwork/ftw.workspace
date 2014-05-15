105d4806f91517f08166edbc67db0fafcfcf8548
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-05-15 14:10:32 +0200

    Extend pdf preview if there's a image generated by collective.pdfpeek.

--- ftw/workspace/browser/previews/office.py
+++ ftw/workspace/browser/previews/office.py
@@ -1,10 +1,11 @@
+from ftw.workspace import _
 from ftw.workspace.browser.previews.default import DefaultPreview
 from ftw.workspace.interfaces import IWorkspacePreview
+from Products.ATContentTypes.interfaces.file import IATFile
 from Products.CMFCore.utils import getToolByName
 from zope.component import adapts
 from zope.interface import implements
 from zope.interface import Interface
-from Products.ATContentTypes.interfaces.file import IATFile
 
 
 class DocPreview(DefaultPreview):
@@ -78,7 +79,31 @@ class PdfPreview(DefaultPreview):
     implements(IWorkspacePreview)
     adapts(IATFile, Interface)
 
+    def __init__(self, context, request):
+        super(PdfPreview, self).__init__(context, request)
+        self.has_image_preview = bool(self.context.restrictedTraverse(
+            'view-image-annotation',
+            None))
+
     def full_url(self):
-        portal_url = getToolByName(self.context, 'portal_url')
-        return '{0}/++resource++ftw.workspace-resources/pdf.png'.format(
-            portal_url())
+        if self.has_image_preview:
+            return '{0}/++images++1_preview'.format(
+                self.context.absolute_url())
+        else:
+            portal_url = getToolByName(self.context, 'portal_url')
+            return '{0}/++resource++ftw.workspace-resources/pdf.png'.format(
+                portal_url())
+
+    def preview(self):
+        if self.has_image_preview:
+            return '<img height="200px" src="{0}" alt={1} title={1}/>'.format(
+                '{0}/++images++1_thumb'.format(
+                    self.context.absolute_url()),
+                self.context.Title()
+            )
+        else:
+            return '<img height="200px" src="{0}" alt={1} title={1}/>'.format(
+                self.full_url(),
+                _(u'text_no_preview', default=u'No Preview')
+                )
+
