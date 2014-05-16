926c581965bf975b5bd71e1bf803d612b0bab2bb
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2014-05-16 10:50:33 +0200

    Test ftw.meeting Meeting content type preview.

--- ftw/workspace/tests/builders.py
+++ ftw/workspace/tests/builders.py
@@ -10,5 +10,10 @@ class TabbedViewFolderBuilder(ArchetypesBuilder):
     portal_type = 'TabbedViewFolder'
 
 
+class MeetingBuilder(ArchetypesBuilder):
+    portal_type = 'Meeting'
+
+
 builder_registry.register('workspace', WorkspaceBuilder)
 builder_registry.register('TabbedViewFolder', TabbedViewFolderBuilder)
+builder_registry.register('meeting', MeetingBuilder)
