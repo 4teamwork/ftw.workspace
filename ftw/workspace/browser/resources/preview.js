9c785afb681752e21096a5fdb4e71de02a393f18
Timon Tschanz <timon.tschanz@gmail.com>
2015-07-24 14:31:21 +0200

    add visited previews to history

--- ftw/workspace/browser/resources/preview.js
+++ ftw/workspace/browser/resources/preview.js
@@ -15,26 +15,57 @@
 
   "use strict";
 
-
+    var currentindex = -1;
     var dontupdatehistory = false;
     var current_state;
     var init = function() {
       $("body").css("overflow", "hidden");
       $("#colorbox").addClass("file-preview-colorbox");
       $("#cboxOverlay").addClass("file-preview-colorbox-background");
-      dontupdatehistory = false;
+      if(currentindex == -1){
+        currentindex = $('.colorboxLink').index($.colorbox.element());
+        }
+    $('#cboxNext').unbind('click');
+    $('#cboxPrevious').unbind('click');
+        $('#cboxNext').on('click', function() {
+            if(currentindex < $('.colorboxLink').length -1) {
+                currentindex++;
+            }
+            else {
+                currentindex = 0;
+            }
+            $.colorbox.next();
+        });
+
+        $('#cboxPrevious').on('click', function() {
+            if(currentindex > 0){
+                currentindex--;
+            }
+            else {
+                currentindex = $('.colorboxLink').length -1;
+            }
+            $.colorbox.prev();
+        });
+
     };
+
 
     var changeUrl = function(){
         if (!current_state){
             current_state = window.location.pathname + window.location.hash;
         }
         var path = $.colorbox.element().attr("href").replace(RegExp("^(" + "http://" + location.host + ")", "g"), "");
-        window.history.replaceState({}, "", path);
+        if (!dontupdatehistory){
+            console.log(currentindex);
+            window.history.pushState({'index': currentindex}, "", path);
+        }
+        else{dontupdatehistory = false;}
+
 
     };
     var cleanup = function() { $("body").css("overflow", "scroll");
-                               window.history.replaceState({}, "", current_state); };
+                               window.history.pushState({}, "", current_state);
+                               currentindex = -1 };
 
     var destroy = function() {  };
 
@@ -65,5 +96,26 @@
       });
       $(".colorboxLink").colorbox(settings);
     });
+
+$(window).on('popstate', function(event) {
+    if(history.state.index == undefined && location.pathname + location.hash != current_state){
+        return;
+    }
+    else if(history.state.index == undefined && location.pathname + location.hash == current_state){
+        $.colorbox.close();
+        return;
+    }
+
+    if((history.state.index < currentindex && !(history.state.index == 0 && currentindex ==$('.colorboxLink').length -1)) || (currentindex ==0 && history.state.index == $('.colorboxLink').length -1)){
+        dontupdatehistory = true;
+        currentindex = history.state.index;
+        $.colorbox.prev();
+    }
+    else{
+        dontupdatehistory = true;
+        currentindex = history.state.index;
+        $.colorbox.next();
+    }
+});
 
 }(jQuery));
